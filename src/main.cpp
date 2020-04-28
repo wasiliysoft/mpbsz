d297fd8e53bd9aa13ab32316ca1c735eeebc6c29
WasiliySoft-desktop <wasiliysoft@gmail.com>
2020-04-28 12:43:31 +0500

    add 30/60 MODULATOR, support swich signal mode

--- src/main.cpp
+++ src/main.cpp
@@ -69,6 +69,8 @@
 */
 #define MAX_UOZ_OFFSET 15
 
+#define MODULATOR 30
+
 //---- НАСТРОЙКА ПИНОВ
 #define PIN_BTN_UOZ_UP 4 // пин кнопки увеличения угла опережения
 #define PIN_BTN_UOZ_DOWN 7 // пин кнопки уменьшения угла опережения
@@ -187,11 +189,16 @@ void setup() {
 void loop() {
   if (g_state == 1) {
 
-    if (g_vmt_mode) {
-      // режим искры в ВМТ по границе выхода шторки
-      if (g_installation_mode == true) {
-        // режим настройки шторки
-        if (digitalRead(3) == HIGH) {
+    if (g_vmt_mode) { // режим искры в ВМТ по границе выхода шторки
+      if (g_installation_mode == true) { // режим настройки шторки
+        static bool beepMode = false;
+        if (digitalRead(PIN_BTN_UOZ_DOWN) == LOW) {
+          // инверсия режима сигнализации шторки
+          beepMode = !beepMode;
+          oneBeep();
+          delay(500);
+        }
+        if (digitalRead(3) == beepMode) {
           digitalWrite(PIN_LED_MODE, HIGH);
           digitalWrite(PIN_BUZZER, HIGH);
         } else {
@@ -211,8 +218,8 @@ void loop() {
         in_count++;
       }
 
-    } else {
-      // нормальный режим работы с УОЗ
+    } else { // нормальный режим работы с УОЗ
+
 #if MIN_TIME_180 != 0
       if (g_time_180 > MIN_TIME_180) {
 #endif
@@ -243,14 +250,15 @@ void loop() {
     // расчет времени полуоборота
     g_time_180 = (unsigned long)(g_cur_time - g_last_time);
 
-    // выбор времени задержки вспышки относительно
-    // ВХОДА шторки для двухлепесткового модулятора,
-    // чем меньше задержка тем больше УОЗ
-    // для 30 - градусной шторки
+// выбор времени задержки вспышки относительно
+// ВХОДА шторки для двухлепесткового модулятора,
+// чем меньше задержка тем больше УОЗ
+// для 30 - градусной шторки
 
-    // ######################################################
-    // ######################################################
-    // СЮДА ВСТАВЛЯЕТСЯ КОД ИЗ ТАБЛИЦЫ РАСЧЕТА УОЗ
+// ######################################################
+// ######################################################
+// СЮДА ВСТАВЛЯЕТСЯ КОД ИЗ ТАБЛИЦЫ РАСЧЕТА УОЗ
+#if MODULATOR == 30
     if (g_time_180 < 5000) {
       g_delay_time = 123; // RPM 6000 UOZ 25,58
     } else if (g_time_180 < 5455) {
@@ -302,6 +310,59 @@ void loop() {
     } else if (g_time_180 < 600000) {
       g_delay_time = 95000; // RPM 50 UOZ 1,5
     }
+#else
+    if (g_time_180 < 5000) {
+      g_delay_time = 956; // RPM 6000 UOZ 25,58
+    } else if (g_time_180 < 5455) {
+      g_delay_time = 1064; // RPM 5500 UOZ 24,88
+    } else if (g_time_180 < 6000) {
+      g_delay_time = 1195; // RPM 5000 UOZ 24,15
+    } else if (g_time_180 < 6667) {
+      g_delay_time = 1356; // RPM 4500 UOZ 23,39
+    } else if (g_time_180 < 7500) {
+      g_delay_time = 1558; // RPM 4000 UOZ 22,6
+    } else if (g_time_180 < 8571) {
+      g_delay_time = 1821; // RPM 3500 UOZ 21,76
+    } else if (g_time_180 < 9231) {
+      g_delay_time = 1996; // RPM 3250 UOZ 21,07
+    } else if (g_time_180 < 10000) {
+      g_delay_time = 2202; // RPM 3000 UOZ 20,37
+    } else if (g_time_180 < 10909) {
+      g_delay_time = 2445; // RPM 2750 UOZ 19,65
+    } else if (g_time_180 < 12000) {
+      g_delay_time = 2740; // RPM 2500 UOZ 18,9
+    } else if (g_time_180 < 13333) {
+      g_delay_time = 3101; // RPM 2250 UOZ 18,13
+    } else if (g_time_180 < 15000) {
+      g_delay_time = 3557; // RPM 2000 UOZ 17,32
+    } else if (g_time_180 < 16216) {
+      g_delay_time = 3910; // RPM 1850 UOZ 16,6
+    } else if (g_time_180 < 17647) {
+      g_delay_time = 4326; // RPM 1700 UOZ 15,87
+    } else if (g_time_180 < 19355) {
+      g_delay_time = 4827; // RPM 1550 UOZ 15,11
+    } else if (g_time_180 < 21429) {
+      g_delay_time = 5437; // RPM 1400 UOZ 14,33
+    } else if (g_time_180 < 24000) {
+      g_delay_time = 6197; // RPM 1250 UOZ 13,52
+    } else if (g_time_180 < 27273) {
+      g_delay_time = 7171; // RPM 1100 UOZ 12,67
+    } else if (g_time_180 < 31579) {
+      g_delay_time = 8461; // RPM 950 UOZ 11,77
+    } else if (g_time_180 < 37500) {
+      g_delay_time = 10250; // RPM 800 UOZ 10,8
+    } else if (g_time_180 < 46154) {
+      g_delay_time = 12890; // RPM 650 UOZ 9,73
+    } else if (g_time_180 < 60000) {
+      g_delay_time = 17170; // RPM 500 UOZ 8,49
+    } else if (g_time_180 < 85714) {
+      g_delay_time = 25271; // RPM 350 UOZ 6,93
+    } else if (g_time_180 < 150000) {
+      g_delay_time = 46158; // RPM 200 UOZ 4,61
+    } else if (g_time_180 < 600000) {
+      g_delay_time = 195000; // RPM 50 UOZ 1,5
+    }
+#endif
     // КОНЕЦ БЛОКА КОДА ИЗ ТАБЛИЦЫ РАСЧЕТА УОЗ
     // ######################################################
     // ######################################################
